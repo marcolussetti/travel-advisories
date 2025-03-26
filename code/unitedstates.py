a2916257bc51f4aac4f481183783d33c8fdc17cd
marcolussetti <git@marcolussetti.com>
2025-03-26 00:00:45 -0700

    unitedstates: wip

--- code/unitedstates.py
+++ code/unitedstates.py
@@ -1,11 +1,11 @@
+import re
+import json
+import csv
 import os
-from time import sleep
 import requests
 from bs4 import BeautifulSoup
-import csv
-import json
 from datetime import datetime
-import re
+from time import sleep
 from markdownify import markdownify as md
 
 BASE_URL = "https://travel.state.gov"
@@ -34,6 +34,11 @@ def sanitize_path_element(path_element):
 
 def parse_advisories(html):
     soup = BeautifulSoup(html, "html.parser")
+
+    # **NEW: Extract country data from JavaScript**
+    country_info_map = extract_country_info_map(soup)
+    country_info_urls = [c.get("url")for c in country_info_map if c.get("url")]
+
     table = soup.find("div", {"class": "table-data"})
     if not table:
         raise ValueError("Could not find advisory table on the page")
@@ -55,7 +60,9 @@ def parse_advisories(html):
             advisory_details_url = BASE_URL + destination_tag["href"]
             risk_level = columns[1].text.strip()
             try:
-                risk_level_number = int(risk_level.split(":")[0].replace("Level", "").strip())
+                risk_level_number = int(
+                    risk_level.split(":")[0].replace("Level", "").strip()
+                )
             except:
                 risk_level_number = -1
 
@@ -72,11 +79,11 @@ def parse_advisories(html):
                     "risk_level": risk_level,
                     "last_updated": last_updated,
                     "advisory_details_url": advisory_details_url,
-                    "risk_level_number": risk_level_number,
+                    "risk_level_number": risk_level_number
                 }
             )
-        except:
-            print("Failed to parse a row!")
+        except Exception as e:  # Catch a broader range of exceptions
+            print(f"Failed to parse a row! {e}")
             try:
                 print(row)
             except:
@@ -87,6 +94,37 @@ def parse_advisories(html):
     return advisories
 
 
+def extract_country_info_map(soup):
+    results = []
+    # try:
+    # Find the script tag containing the country data
+    script_tag = soup.select("div.searchCSI script")
+    if not script_tag:
+        print("Could not find script tag inside div.searchCSI")
+        return results
+
+    script_content = script_tag.string
+    if not script_content:
+        print("Script tag is empty.")
+        return results
+
+    for line in script_content.splitlines():
+        line = line.strip()
+        if line.startswith("{"):  # Only process lines with this value
+            data = {}
+            parts = line[1:-1].split(",")
+            for part in parts:
+                if ":" not in part:
+                    continue
+                key, value = part.split(":", 1)
+                key = key.strip().strip("'").strip('"')
+                value = value.strip().strip("'").strip('"')
+                data[key] = value
+            results.append(data)
+    print(results)
+    return results
+
+
 def parse_advisory_details(html, destination):
     soup = BeautifulSoup(html, "html.parser")
 
@@ -95,7 +133,9 @@ def parse_advisory_details(html, destination):
     if advisory_content_frame:
         advisory_content = md(str(advisory_content_frame), heading_style="ATX")
         os.makedirs(f"unitedstates/{destination}", exist_ok=True)
-        with open(f"unitedstates/{destination}/Advisory.md", "w", encoding="utf-8") as f:
+        with open(
+            f"unitedstates/{destination}/Advisory.md", "w", encoding="utf-8"
+        ) as f:
             f.write(advisory_content)
     else:
         print(f"Could not find advisory content frame for {destination}")
@@ -103,7 +143,10 @@ def parse_advisory_details(html, destination):
 
     # Extract country details URL
     country_details_url = None
-    country_info_link = advisory_content_frame.find("a", string=lambda text: text and "country information page" in text.lower())
+    country_info_link = advisory_content_frame.find(
+        "a",
+        string=lambda text: text and "country information page" in text.lower(),
+    )
     if country_info_link:
         country_details_url = country_info_link["href"]
     else:
@@ -116,12 +159,18 @@ def parse_country_details(html, destination):
     soup = BeautifulSoup(html, "html.parser")
 
     # Find sections
-    sections_container = soup.select_one(".tsg-rwd-main-CSI-International-Travel-items-international")
+    sections_container = soup.select_one(
+        ".tsg-rwd-main-CSI-International-Travel-items-international"
+    )
     if not sections_container:
         print(f"Could not find main CSI container for {destination}")
         return {}
-    sections = [section for section in sections_container.find_all("div", recursive=False)
-                if "tsg-rwd-accordion-nav-frame-for-freestanding-all-buttons-csi-show" not in section.get("class", [])]
+    sections = [
+        section
+        for section in sections_container.find_all("div", recursive=False)
+        if "tsg-rwd-accordion-nav-frame-for-freestanding-all-buttons-csi-show"
+        not in section.get("class", [])
+    ]
 
     details = {}
     for section in sections:
@@ -129,7 +178,9 @@ def parse_country_details(html, destination):
         if header:
             section_name = header.text.strip()
             sanitized_section_name = sanitize_path_element(section_name)
-            section_id = section_name.replace(" ", "_").replace("/", "_").lower()  # Create a unique ID
+            section_id = (
+                section_name.replace(" ", "_").replace("/", "_").lower()
+            )  # Create a unique ID
             section_content = md(str(section), heading_style="ATX")
 
             filepath = f"unitedstates/{destination}/{sanitized_section_name}.md"
@@ -147,7 +198,15 @@ def parse_country_details(html, destination):
 def save_to_csv(data, filename):
     with open(filename, "w", newline="", encoding="utf-8") as f:
         writer = csv.DictWriter(
-            f, fieldnames=["destination", "risk_level", "last_updated", "advisory_details_url", "risk_level_number"]
+            f,
+            fieldnames=[
+                "destination",
+                "risk_level",
+                "last_updated",
+                "advisory_details_url",
+                "risk_level_number",
+                "country_details_url",
+            ],
         )
         writer.writeheader()
         writer.writerows(data)
@@ -158,20 +217,34 @@ def save_to_json(data, filename):
         json.dump(data, f, indent=4)
 
 
-def run_country_details(advisory_details_url, destination, last_updated):
+def run_country_details(
+    advisory_details_url, destination, last_updated, country_details_url_from_js
+):
     print(f"Fetching advisory details for {destination} from {advisory_details_url}")
     try:
         html = fetch_page(advisory_details_url)
-        country_details_url = parse_advisory_details(html, destination)
+        # country_details_url = parse_advisory_details(html, destination)  #No need to parse again, using value from JS.
 
         details = {}
-        if country_details_url:
+
+        if country_details_url_from_js:
+            country_details_url = country_details_url_from_js
             print(f"Fetching country details for {destination} from {country_details_url}")
             html = fetch_page(country_details_url)
             details = parse_country_details(html, destination)
-
         else:
-            print(f"No country details to fetch for {destination}")
+            country_details_url = parse_advisory_details(
+                html, destination
+            )  # If JS version is not available, use find via parsing
+
+            if country_details_url:
+                print(
+                    f"Fetching country details for {destination} from {country_details_url}"
+                )
+                html = fetch_page(country_details_url)
+                details = parse_country_details(html, destination)
+            else:
+                print(f"No country details to fetch for {destination}")
 
         # Create the 'info.json' file
         pages = []
@@ -182,13 +255,11 @@ def run_country_details(advisory_details_url, destination, last_updated):
             "name": destination,
             "risk_level": None,  # You'll need to fetch risk level from somewhere
             "pages": pages,
-            "update_date": last_updated
+            "update_date": last_updated,
         }
-
 
         with open(f"unitedstates/{destination}/info.json", "w", encoding="utf-8") as f:
             json.dump(info, f, indent=4)
-
 
     except ValueError as e:
         print(f"Failed to fetch details for {destination}: {e}")
@@ -199,15 +270,26 @@ def run_all_countries(since: datetime = None):
         countries_summary = json.load(f)
 
     targets = [
-        (country["advisory_details_url"], country["destination"], country["last_updated"])
+        (
+            country["advisory_details_url"],
+            country["destination"],
+            country["last_updated"],
+            country["country_details_url"],
+        )
         for country in countries_summary
-        if since is None or (country["last_updated"] and datetime.fromisoformat(country["last_updated"]) >= since)
+        if since is None
+        or (country["last_updated"] and datetime.fromisoformat(country["last_updated"]) >= since)
     ]
 
-    for advisory_details_url, destination, last_updated in targets:
+    for advisory_details_url, destination, last_updated, country_details_url_from_js in targets:
         try:
             destination_sanitized = sanitize_path_element(destination)
-            run_country_details(advisory_details_url, destination_sanitized, last_updated)
+            run_country_details(
+                advisory_details_url,
+                destination_sanitized,
+                last_updated,
+                country_details_url_from_js,
+            )
         except ValueError as e:
             print(f"Failed to fetch {destination}: {e}")
         sleep(DETAILS_PARSING_DELAY)
